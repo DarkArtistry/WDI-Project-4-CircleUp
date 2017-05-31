cb3028d401d4408f0f75324fc6eab0ae84dd9cef
zhenhao goh <kenneth.gzhao@outlook.com>
2017-05-31 09:18:34 +0800

    change industry controller

--- app/controllers/industries_controller.rb
+++ app/controllers/industries_controller.rb
@@ -12,40 +12,32 @@ def show
 
    @allIndustryPost = []
    @allIndustryUser = User.where(industry: @Title)
-
-   if @allIndustryUser.length > 0
-
-     @allIndustryUser.each do |user|
-       @allIndustryPost += user.posts
-     end
+   @allIndustryUser.each do |user|
+     @allIndustryPost += user.posts
    end
-
    @allIndustryPost.sort
 
-   if @allIndustryPost.length > 0
-
-     @Post = JSON.parse(@allIndustryPost.to_json)
-     @allPostlikes = JSON.parse(Postlike.all.to_json)
-
-     @Post.each do |post|
-       @allPostComment = JSON.parse(Comment.where(post_id: post["id"]).to_json)
-       @allPostComment.each do |comment|
-         @commentUser = User.where(id: comment["user_id"])[0]
-         comment["username"] = @commentUser.firstname
-         @postAuthor = User.where(id: post["author_id"])[0]
-         @postShareUser = User.where(id: post["user_id"])[0]
-         post["comments"] = @allPostComment
-         post["author"] = @postAuthor[:firstname]
-         post["shareuser"] = @postShareUser[:firstname]
-         post["authorid"] = @postAuthor[:id]
-         post["shareuserid"] = @postShareUser[:id]
-         @allPost = @Post
-       end
-
+   @Post = JSON.parse(@allIndustryPost.to_json)
+   @allPostlikes = JSON.parse(Postlike.all.to_json)
+   @Post.each do |post|
+     @allPostComment = JSON.parse(Comment.where(post_id: post["id"]).to_json)
+     @allPostComment.each do |comment|
+       @commentUser = User.where(id: comment["user_id"])[0]
+       comment["username"] = @commentUser.firstname
+       # puts comment.inspect
+       # puts 'please'
      end
 
+     @postAuthor = User.where(id: post["author_id"])[0]
+     @postShareUser = User.where(id: post["user_id"])[0]
+     post["comments"] = @allPostComment
+     post["author"] = @postAuthor[:firstname]
+     post["shareuser"] = @postShareUser[:firstname]
+     post["authorid"] = @postAuthor[:id]
+     post["shareuserid"] = @postShareUser[:id]
    end
-   @targetUser = JSON.parse(User.where(id: params["id"])[0].to_json)
+   @allPost = @Post
+  
 
    end
 
